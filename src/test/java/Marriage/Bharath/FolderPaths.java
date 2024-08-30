332e80d676f90b405e1740b45250cad55b5f9c40
rajavenkatasaikiran_ <rajavenkatasaikiran_jakkula@epam.com>
2024-08-30 18:05:46 +0530

    changes

--- src/test/java/Marriage/Bharath/FolderPaths.java
+++ src/test/java/Marriage/Bharath/FolderPaths.java
@@ -4,7 +4,7 @@ public interface FolderPaths {
     String PDF ="C:/Users/rajavenkatasaikiran_/IdeaProjects/TestAutomation/target/pdfs";
     String SCREENSHOTS = "C:/Users/rajavenkatasaikiran_/IdeaProjects/TestAutomation/target/screenshots/";
     String EXCEL = "C:/Users/rajavenkatasaikiran_/IdeaProjects/TestAutomation/target/Reports";
-    String HTML = "C:/Users/rajavenkatasaikiran_/IdeaProjects/TestAutomation/target/Reports";
+    String HTML = "C:/Users/rajavenkatasaikiran_/IdeaProjects/TestAutomation/src/test/java/Marriage/pdfExcelImg";
 
 
 }
