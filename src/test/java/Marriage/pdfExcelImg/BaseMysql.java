3f8e95e800def4606bc2e3aab7ed31d694eec1de
jrvsaikiran <saikiranjrv@gmail.com>
2024-09-22 20:30:43 +0530

    bharath

--- src/test/java/Marriage/pdfExcelImg/BaseMysql.java
+++ src/test/java/Marriage/pdfExcelImg/BaseMysql.java
@@ -3,11 +3,15 @@ package Marriage.pdfExcelImg;
 import Marriage.Bharath.ProfileData;
 
 import java.sql.*;
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
 
 public class BaseMysql {
 
     private static Connection con;
+    private static int i=1;
+    private String name;
 
 //    public static void main(String[] arg) {
 //        mysqlDb();
@@ -16,6 +20,13 @@ public class BaseMysql {
 
     public  void insert(List<ProfileData> list) {
         ProfileData profileData = list.get(0);
+         name = profileData.getName().trim().replaceAll("[^a-zA-Z0-9]+", "").trim();
+        String age = profileData.getAge().trim().replaceAll("[^a-zA-Z0-9]+","").trim();
+        String cast = profileData.getCast().trim().replaceAll("[^a-zA-Z0-9]+","").trim();
+        String education = profileData.getEducation().trim().replaceAll("[^a-zA-Z0-9]+","").trim();
+        String location = profileData.getLocation().trim().replaceAll("[^a-zA-Z0-9]+","").trim();
+        String activity = profileData.getActivity().trim().replaceAll("[^a-zA-Z0-9]+","").trim();
+        String date = timeMethod();
 
         try {
             if (con == null) {
@@ -34,19 +45,47 @@ public class BaseMysql {
 //                  \s""";
 
             String insert = "INSERT INTO" + " `jrvdb`.`bharathprofiles`" +
-                    "(`name`, `age`, `cast`, `education`, `location`, `activity`)" +
-                    " VALUES " + "(" + "'" + profileData.getName().trim() + "', " + "'" + profileData.getAge().trim() + "', " +
-                    "'" + profileData.getCast().trim() + "', " + "'" + profileData.getEducation().trim() + "', " +
-                    "'" + profileData.getLocation().trim() + "', " + "'" + profileData.getActivity().trim() + "'); ";
-            int i = statement.executeUpdate(insert);
-            System.out.println("Successfully Inserted " + i + " records");
+                    "(`name`, `age`, `cast`, `education`, `location`, `activity`, `date`)" +
+                    " VALUES " + "(" + "'" + name + "', " + "'" + age + "', " +
+                    "'" +cast + "', " + "'" + education+ "', " +
+                    "'" + location + "', " + "'" + activity + "', " + "'" + date + "'); ";
+            String insert1 = "INSERT INTO" + " `jrvdb`.`bharathprofiles`" +
+                    "(`name`)" +
+                    " VALUES " + "(" + "'" + profileData.getName().trim() +"'"+");";
 
+            try {
+                int record = statement.executeUpdate(insert);
+                System.out.println("Successfully Inserted " + record + " records");
+            } catch (Exception e) {
+
+                name = name + i;
+                String insert2 = "INSERT INTO" + " `jrvdb`.`bharathprofiles`" +
+                        "(`name`, `age`, `cast`, `education`, `location`, `activity`)" +
+                        " VALUES " + "(" + "'" + name + "', " + "'" + age + "', " +
+                        "'" +cast + "', " + "'" + education+ "', " +
+                        "'" + location + "', " + "'" + activity + "', " + "'" + date + "'); ";
+                int record = statement.executeUpdate(insert2);
+                System.out.println("Successfully Inserted " + record + " records");
+                i++;
+
+            }
 
 
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
 
+    }
+
+    private String timeMethod() {
+        LocalDate date = LocalDate.now();
+        String s1 = String.valueOf(date);
+
+        LocalTime time = LocalTime.now();
+        String s = String.valueOf(time);
+        String[] split = s.split("\\.");
+        String s2 = split[0].replace(":", "-");
+        return s1 +" "+ s2;
     }
 
     public static void mysqlDb() {
@@ -65,4 +104,6 @@ public class BaseMysql {
             throw new RuntimeException(e);
         }
     }
+
+
 }
